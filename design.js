d35f002938a1cd9d5aad82810166533bfbdd532c
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-07-10 19:52:42 +0530

    Initial commit

--- design.js
+++ design.js
@@ -367,3 +367,20 @@
             document.getElementById("c").innerHTML = "Moves : "+ m.getMoves()
         }
        setInterval(drawMoves, 100);
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
